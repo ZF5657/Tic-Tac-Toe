722c2251f9ce44fc339e064a22cfec1a86cb9b5a
ZF5657 <zane56fields57@gmail.com>
2024-03-04 14:47:52 -0500

    Add loop and event listener for game grid squares

--- script.js
+++ script.js
@@ -8,26 +8,50 @@ const player2 = addPlayer("Jack", 'O');
 console.log({name: player1.name, playerLetter: player1.letter});
 console.log({name: player2.name, playerLetter: player2.letter});
 
-const gameBoard = () => {
-    let a1 = '';
-    let a2 = '';
-    let a3 = '';
-    let b1 = '';
-    let b2 = '';
-    let b3 = '';
-    let c1 = '';
-    let c2 = '';
-    let c3 = '';
-    const gameGrid = [
-        a1, b1, c1, 
-        a2, b2, c2, 
-        a3, b3, c3
-    ];
-    return {gameGrid};
-}
 
 const playGame = () => {
+    const gameGrid = [
+        '', '', '', 
+        '', '', '', 
+        '', '', ''
+    ];
+
+    let squares = document.querySelectorAll('.square');
+    const a1 = document.querySelector('#a1');
+    const a2 = document.querySelector('#a2');
+    const a3 = document.querySelector('#a3');
+    const b1 = document.querySelector('#b1');
+    const b2 = document.querySelector('#b2');
+    const b3 = document.querySelector('#b3');
+    const c1 = document.querySelector('#c1');
+    const c2 = document.querySelector('#c2');
+    const c3 = document.querySelector('#c3');
     
+    squares.forEach((square) => {
+        square.addEventListener('click', () => {
+            if(square == a1) {
+                console.log('a1');
+            } else if(square == b1) {
+                console.log('b1');
+            } else if(square == c1) {
+                console.log('c1');
+            } else if(square == a2) {
+                console.log('a2');
+            } else if(square == b2) {
+                console.log('b2');
+            } else if(square == c2) {
+                console.log('c2');
+            } else if(square == a3) {
+                console.log('a3');
+            } else if(square == b3) {
+                console.log('b3');
+            } else if(square == c3) {
+                console.log('c3');
+            } else {
+                return
+            }
+        })
+    })
 }
 
-gameBoard();
+playGame();
