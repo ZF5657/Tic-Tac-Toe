70280693a25d83ac219dc66c80315ff8fec7992f
ZF5657 <zane56fields57@gmail.com>
2024-03-27 13:26:15 -0400

    Fix determineWinner() and separate out each function

--- script.js
+++ script.js
@@ -22,148 +22,131 @@ document.querySelector('#submit').addEventListener('click', (e) => {
     document.querySelector('.nameInput').style = ('display: none;')
     document.querySelector('.display').textContent = `${player1.name}'s turn`;
     document.querySelector('.display').style = ('font-size: 2em;');
-    playGame();
 })
 
-const playGame = () => {
-    const gameGrid = [
-        '', '', '', 
-        '', '', '', 
-        '', '', ''
-    ];
+const gameGrid = ['', '', '', '', '', '', '', '', ''];
 
+let currentPlayer = '';
+let playerTurn = 1;
+let gameOver = false;
 
-    // let squares = document.querySelectorAll('.square');
-    // const a1 = document.querySelector('#a1');
-    // const a2 = document.querySelector('#a2');
-    // const a3 = document.querySelector('#a3');
-    // const b1 = document.querySelector('#b1');
-    // const b2 = document.querySelector('#b2');
-    // const b3 = document.querySelector('#b3');
-    // const c1 = document.querySelector('#c1');
-    // const c2 = document.querySelector('#c2');
-    // const c3 = document.querySelector('#c3');
-
-    let currentPlayer = '';
-    let playerTurn = 1;
-    let gameOver = false;
-
-    //Function to take turns switching from player 1 to player 2, starting with player 1. Display div displays whose turn it is once the current player has just taken a turn.
-    const takeTurns = () => {
-        if(playerTurn === 1) {
-            currentPlayer = player1.letter;
-            document.querySelector('.display').textContent = `${player2.name}'s turn`;
-            playerTurn = 0;
-        } else if(playerTurn === 0) {
-            currentPlayer = player2.letter;
-            document.querySelector('.display').textContent = `${player1.name}'s turn`;
-            playerTurn = 1;
-        } else {
-            return;
-        }
+//Function to take turns switching from player 1 to player 2, starting with player 1. Display div displays whose turn it is once the current player has just taken a turn.
+const takeTurns = () => {
+    if(playerTurn === 1) {
+        currentPlayer = player1.letter;
+        document.querySelector('.display').textContent = `${player2.name}'s turn`;
+        playerTurn = 0;
+    } else if(playerTurn === 0) {
+        currentPlayer = player2.letter;
+        document.querySelector('.display').textContent = `${player1.name}'s turn`;
+        playerTurn = 1;
+    } else {
+        return;
     }
-
-    const addLetter = () => {
-        let squares = document.querySelectorAll('.square');
-        squares.forEach((square) => {
-
-            const a1 = document.querySelector('#a1');
-            const a2 = document.querySelector('#a2');
-            const a3 = document.querySelector('#a3');
-            const b1 = document.querySelector('#b1');
-            const b2 = document.querySelector('#b2');
-            const b3 = document.querySelector('#b3');
-            const c1 = document.querySelector('#c1');
-            const c2 = document.querySelector('#c2');
-            const c3 = document.querySelector('#c3');
-
-            const determineWinner = () => {
-                if(gameGrid[0] != '' && gameGrid[0] == gameGrid[1] && gameGrid[2]) {
-                    console.log('game over')
-                    gameOver = true;
-                    return null
-                } else if(gameGrid[3] != '' && gameGrid[3] == gameGrid[4] && gameGrid[5]) {
-                    console.log('game over')
-                    gameOver = true;
-                    return null
-                } else if(gameGrid[6] != '' && gameGrid[6] == gameGrid[7] && gameGrid[8]) {
-                    console.log('game over')
-                    gameOver = true;
-                    return null
-                } else if(gameGrid[0] != '' && gameGrid[0] == gameGrid[3] && gameGrid[6]) {
-                    console.log('game over')
-                    gameOver = true;
-                    return null
-                } else if(gameGrid[1] != '' && gameGrid[1] == gameGrid[4] && gameGrid[7]) {
-                    console.log('game over')
-                    gameOver = true;
-                    return null
-                } else if(gameGrid[2] != '' && gameGrid[2] == gameGrid[5] && gameGrid[8]) {
-                    console.log('game over')
-                    gameOver = true;
-                    return null
-                } else if(gameGrid[0] != '' && gameGrid[0] == gameGrid[4] && gameGrid[8]) {
-                    console.log('game over')
-                    gameOver = true;
-                    return null
-                } else if(gameGrid[6] != '' && gameGrid[6] == gameGrid[4] && gameGrid[2]) {
-                    console.log('game over')
-                    gameOver = true;
-                    return null
-                }
-            }
-
-            square.addEventListener('click', () => {
-                determineWinner()
-                //Locks in div display and array if div has been clicked; prevents takeTurns and turn display from changing if clicking on filled square
-                if(square.textContent != '') {
-                    return
-                } else if(square.textContent == '' && gameOver == false) {
-                    takeTurns()
-                    if(square == a1) {
-                        gameGrid[0] = currentPlayer;
-                        a1.textContent = currentPlayer;
-
-                    } else if(square == b1) {
-                        gameGrid[1] = currentPlayer;
-                        b1.textContent = currentPlayer;
-
-                    } else if(square == c1) {
-                        gameGrid[2] = currentPlayer;
-                        c1.textContent = currentPlayer;
-
-                    } else if(square == a2) {
-                        gameGrid[3] = currentPlayer;
-                        a2.textContent = currentPlayer;
-
-                    } else if(square == b2) {
-                        gameGrid[4] = currentPlayer;
-                        b2.textContent = currentPlayer;
-
-                    } else if(square == c2) {
-                        gameGrid[5] = currentPlayer;
-                        c2.textContent = currentPlayer;
-
-                    } else if(square == a3) {
-                        gameGrid[6] = currentPlayer;
-                        a3.textContent = currentPlayer;
-
-                    } else if(square == b3) {
-                        gameGrid[7] = currentPlayer;
-                        b3.textContent = currentPlayer;
-
-                    } else if(square == c3) {
-                        gameGrid[8] = currentPlayer;
-                        c3.textContent = currentPlayer;
-
-                    }
-                    console.log(gameGrid);
-                } else {
-                    return null
-                }
-            })
-
-        })
-    }
-    addLetter()
 }
+
+const determineWinner = () => {
+    if(gameGrid[0] != '' && gameGrid[0] == gameGrid[1] && gameGrid[2] == gameGrid[1]) {
+        console.log('game over')
+        gameOver = true;
+        return null
+    } else if(gameGrid[3] != '' && gameGrid[3] == gameGrid[4] && gameGrid[5] == gameGrid[4]) {
+        console.log('game over')
+        gameOver = true;
+        return null
+    } else if(gameGrid[6] != '' && gameGrid[6] == gameGrid[7] && gameGrid[8] == gameGrid[7]) {
+        console.log('game over')
+        gameOver = true;
+        return null
+    } else if(gameGrid[0] != '' && gameGrid[0] == gameGrid[3] && gameGrid[6] == gameGrid[3]) {
+        console.log('game over')
+        gameOver = true;
+        return null
+    } else if(gameGrid[1] != '' && gameGrid[1] == gameGrid[4] && gameGrid[7] == gameGrid[4]) {
+        console.log('game over')
+        gameOver = true;
+        return null
+    } else if(gameGrid[2] != '' && gameGrid[2] == gameGrid[5] && gameGrid[8] == gameGrid[5]) {
+        console.log('game over')
+        gameOver = true;
+        return null
+    } else if(gameGrid[0] != '' && gameGrid[0] == gameGrid[4] && gameGrid[8] == gameGrid[4]) {
+        console.log('game over')
+        gameOver = true;
+        return null
+    } else if(gameGrid[6] != '' && gameGrid[6] == gameGrid[4] && gameGrid[2] == gameGrid[4]) {
+        console.log('game over')
+        gameOver = true;
+        return null
+    } else if(gameGrid[0] && gameGrid[1] && gameGrid[2] && gameGrid[3] && gameGrid[4] && gameGrid[5] && gameGrid[6] && gameGrid[7] && gameGrid[8] != ''){
+        console.log('Draw')
+        return null
+    }
+}
+
+    let squares = document.querySelectorAll('.square');
+
+    squares.forEach((square) => {
+        determineWinner();
+        const a1 = document.querySelector('#a1');
+        const a2 = document.querySelector('#a2');
+        const a3 = document.querySelector('#a3');
+        const b1 = document.querySelector('#b1');
+        const b2 = document.querySelector('#b2');
+        const b3 = document.querySelector('#b3');
+        const c1 = document.querySelector('#c1');
+        const c2 = document.querySelector('#c2');
+        const c3 = document.querySelector('#c3');
+
+        square.addEventListener('click', () => {
+            //Locks in div display and array if div has been clicked; prevents takeTurns and turn display from changing if clicking on filled square
+            determineWinner()
+            if(square.textContent != '') {
+                return
+            } else if(square.textContent == '' && gameOver == false) {
+                takeTurns()
+                if(square == a1) {
+                    gameGrid[0] = currentPlayer;
+                    a1.textContent = currentPlayer;
+
+                } else if(square == b1) {
+                    gameGrid[1] = currentPlayer;
+                    b1.textContent = currentPlayer;
+
+                } else if(square == c1) {
+                    gameGrid[2] = currentPlayer;
+                    c1.textContent = currentPlayer;
+
+                } else if(square == a2) {
+                    gameGrid[3] = currentPlayer;
+                    a2.textContent = currentPlayer;
+
+                } else if(square == b2) {
+                    gameGrid[4] = currentPlayer;
+                    b2.textContent = currentPlayer;
+
+                } else if(square == c2) {
+                    gameGrid[5] = currentPlayer;
+                    c2.textContent = currentPlayer;
+
+                } else if(square == a3) {
+                    gameGrid[6] = currentPlayer;
+                    a3.textContent = currentPlayer;
+
+                } else if(square == b3) {
+                    gameGrid[7] = currentPlayer;
+                    b3.textContent = currentPlayer;
+
+                } else if(square == c3) {
+                    gameGrid[8] = currentPlayer;
+                    c3.textContent = currentPlayer;
+
+                }
+                console.log(gameGrid);
+            } else {
+                return null
+            }
+        })
+
+    })
+
